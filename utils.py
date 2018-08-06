9f2db723fdd6401364aeec16e2df68f56cb15f52
Dexter Ford <dexter.ford.77@gmail.com>
2018-08-06 04:59:28 -0500

    Task 2 - refactory/repair buggy/inefficient create_email_body method; add comments within method noting further change details

--- utils.py
+++ utils.py
@@ -9,14 +9,16 @@ def validate_commissions(num_of_commissions):
 
 
 def create_email_body(path):
-    def to_dec(s): return Decimal('{}.{}'.format(s[:-2], s[len(s)-2:]))
+    # method was not only inefficient, but actually buggy/broken as well
+    # removed unnecessary space on left in "trans_amount_lines" by more accurately defining location of commission amount in file (ie, at index 29 rather than 28)
+    # fixed error that disincludes last commission amount from total by changing line index cutoff from "-2" to "-1"
+    # convert string to float early on, making "lstrip(0)" method unnecessary
+    # removed unnecessary "to_dec" method; convert total to accurate decimal place by using simple math instead
+    # final version much more readable, clear; unnecessary extra methods & iteration successfully refactored into simpler step-by-step code; major bug fixed
     with path.open() as f:
-        # 28:39 contains the transaction amount
-        trans_amount_lines = [line[28:39] for line in f][2:-2]
-    decimal_converted_lines = list()
-    for line in trans_amount_lines:
-        s = line.lstrip('0')
-        decimal_converted_lines.append(to_dec(s))
+        # 29:39 contains the transaction amount
+        trans_amount_lines = [line[29:39] for line in f][2:-1]
+    decimal_converted_lines = [float(i)/100 for i in trans_amount_lines]
     summed_lines = sum(decimal_converted_lines)
     todays_date = date.today()
     body_format = {
