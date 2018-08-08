e4c1507158d1df369362bb6825d3e54901dbb80e
Dexter Ford <dexter.ford.77@gmail.com>
2018-08-07 23:55:05 -0500

    Task 3 - write docs, logs, and tests

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 from utils import create_email_body, get_num_of_commissions, validate_commissions
 import click
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
 @click.command()
@@ -12,8 +16,10 @@ def main(file_path: str):
     :param file_path: relative path to the file to read
     :return: contents of an email that could be sent
     """
+    log.info('Reading commission file...')
     path_obj = Path(file_path)
     num_of_commissions = get_num_of_commissions(path_obj)
+    log.info('Validating file...')
     commission_is_valid = validate_commissions(num_of_commissions)
     email_body = create_email_body(path_obj) if commission_is_valid else 'Failed to validate'
 
---
+++ test_utils.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import unittest
+from utils import create_email_body, get_num_of_commissions
+
+
+class TestUtils(unittest.TestCase):
+  """Tests for utils.py"""
+
+
+  def setUp(self):
+    self.good_file = Path("examples/good_file.txt")
+    self.corrupt_file = Path("examples/corrupt.txt")
+
+
+  def test_get_num_of_commissions(self):
+
+    # if given valid input, it returns an integer
+    self.assertEqual(type(get_num_of_commissions(self.good_file)), type(1))
+
+    # if given corrupt input, it raises an error
+    with self.assertRaises(ValueError):
+      get_num_of_commissions(self.corrupt_file)
+
+
+  def test_create_email_body(self):
+
+    # it returns a string
+    self.assertEqual(type(create_email_body(self.good_file)), type("This is a string."))
+
+    # the returned string contains the correct total amount, implying that all of the transaction lines have been summed correctly
+    body = create_email_body(self.good_file)
+    result = body.find("353.7")
+    self.assertNotEqual(result, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
